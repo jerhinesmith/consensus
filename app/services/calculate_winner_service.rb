b3f3e94ea471513e97ffc8b5bc12dd0145dc8d6f
Justin Rhinesmith <jerhinesmith@gmail.com>
2015-12-11 17:53:08 -0800

    Minor change

--- app/services/calculate_winner_service.rb
+++ app/services/calculate_winner_service.rb
@@ -10,22 +10,28 @@ class CalculateWinnerService
   def call
     votes    = @topic.votes.collect(&:suggestion_ids)
     majority = (votes.length + 1) / 2
+    winner   = nil
 
-    first_place_votes = Hash.new(0)
+    while !winner
+      first_place_votes = Hash.new(0)
 
-    votes.each do |suggestion_ids|
-      first_place_votes[suggestion_ids.first] += 1
+      votes.each do |suggestion_ids|
+        first_place_votes[suggestion_ids.first] += 1
+      end
+
+      sorted_votes = first_place_votes.sort_by{|_, v| -1 * v}
+
+      top = sorted_votes.first
+
+      if top[1] > majority
+        winner = top[0]
+      else
+        loser = sorted_votes.last[0]
+        votes.each{|suggestion_ids| suggestion_ids.delete loser}
+      end
     end
 
-    first_place_votes.sort_by!{|_, v| v}
-
-    top = first_place_votes.first
-
-    if top[1] > majority
-      winner = top[0]
-    else
-      loser =
-    end
+    puts winner
   end
 
   def self.call(topic)
